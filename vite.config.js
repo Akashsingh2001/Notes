f9de5ce8baea6c9127ed0a7a6ad6bc1342306c5d
Todo_application_boot <akash193258@gmail.com>
2025-09-08 01:53:39 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/notes': {
-        target: 'https://quicknotes-production.up.railway.app',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  build: {
-    outDir: 'build',
-  },
+  base: '/',  // Keep '/' if deploying at root
 });
